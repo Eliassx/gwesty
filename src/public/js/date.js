cb9a49b227fac24c491876fda65f5526f9d4034a
Guilherme <guieliasmonteiro@gmail.com>
2023-12-01 11:40:36 -0300

    finished diagram

--- src/public/js/date.js
+++ src/public/js/date.js
@@ -1,17 +1,18 @@
 const payValue = document.getElementById("pay_value");
 
 function calculate() {
-    const valueReserve = document.getElementById("reserve_value");
+    const antecipationValue = Number(document.getElementById("anticipation_value").value);
+    const comsuptionValue = Number(document.getElementById("comsuption_value").value);
+    const valueReserve = Number(document.getElementById("reserve_value").value);
     const checkIn = new Date(document.getElementById("join_effective").value);
     const checkOut = new Date(document.getElementById("leave_effective").value);
 
-    if(valueReserve.value && checkIn && checkOut) {
+    if(valueReserve && checkIn && checkOut) {
         const diff = checkOut.getTime() - checkIn.getTime();
     
         const diffDays = diff / (1000 * 3600 * 24);
     
-        console.log(diff);
-
-        payValue.value = valueReserve.value * diffDays;
+        
+        payValue.value = valueReserve * diffDays + comsuptionValue - antecipationValue ;
     }    
 }
